be6ab8e75f7d7a37b0cdca75016b4d7f8783acde
Luke Masterson <info@herosgymtslc.com>
2024-05-29 20:26:16 +0000

    Created and wired up About app

--- codestar/settings.py
+++ codestar/settings.py
@@ -42,8 +42,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Apps added
     'django_summernote',
     'blog',
+    'about',
 ]
 
 MIDDLEWARE = [
